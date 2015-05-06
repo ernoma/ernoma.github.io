7032d4471342868c623d8ec0d0444532d1cc5cce
ernoma <ernoma@gmail.com>
2015-05-06 16:25:30 +0300

    Add posibility to choose the basemap

--- js/heat_tamperenoise.js
+++ js/heat_tamperenoise.js
@@ -2,19 +2,61 @@
 var INITIAL_LAT = 61.5;
 var INITIAL_LON = 23.766667;
 
-var mmlTaustaLayer1 = L.tileLayer('http://tiles.kartat.kapsi.fi/taustakartta/{z}/{x}/{y}.jpg', {
-    maxZoom: 17
-});
-var mmlTaustaLayer2 = L.tileLayer('http://tiles.kartat.kapsi.fi/taustakartta/{z}/{x}/{y}.jpg', {
-    maxZoom: 17
-});
-var mmlTaustaLayer3 = L.tileLayer('http://tiles.kartat.kapsi.fi/taustakartta/{z}/{x}/{y}.jpg', {
-    maxZoom: 17
-});
-var mmlTaustaLayer4 = L.tileLayer('http://tiles.kartat.kapsi.fi/taustakartta/{z}/{x}/{y}.jpg', {
-    maxZoom: 17
-});
+var baseLayersOfMaps = [[], [], [], []];
+var currentBaseLayerIndex = 0;
 
+for (var i = 0; i < baseLayersOfMaps.length; i++) {
+    var osmLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+	maxZoom: 17
+    });
+    baseLayersOfMaps[i].push(osmLayer);
+    var osmCycleLayer = L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Maps &copy; <a href="http://thunderforest.com/">Thunderforest</a>',
+	maxZoom: 17
+    });
+    baseLayersOfMaps[i].push(osmCycleLayer);
+    var mapQuestLayer = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
+	subdomains: '1234',
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
+	maxZoom: 17
+    });
+    baseLayersOfMaps[i].push(mapQuestLayer);
+    var stamenTonerLayer = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.jpg', {
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>. Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>',
+	maxZoom: 17
+    });
+    baseLayersOfMaps[i].push(stamenTonerLayer);
+    var mmlOrtoLayer = L.tileLayer('http://tiles.kartat.kapsi.fi/ortokuva/{z}/{x}/{y}.jpg', {
+	attribution: 'Sisältää Maanmittauslaitoksen ortoilmakuva-aineistoa, <a href="http://www.maanmittauslaitos.fi/avoindata_lisenssi_versio1_20120501">lisenssi</a>, TMS: <a href="http://kartat.kapsi.fi/">kartat.kapsi.fi</a>',
+	maxZoom: 17
+    });
+    baseLayersOfMaps[i].push(mmlOrtoLayer);
+    var treGuideLayer = L.tileLayer.wms('http://opendata.navici.com/tampere/ows?service=wms', {
+	attribution: 'Sisältää Tampereen kaupungin <a href="http://palvelut2.tampere.fi/tietovaranto/tietovaranto.php?id=136&alasivu=1&vapaasana=">"Tampereen opaskartta"</a>-aineistoa, <a href="http://www.tampere.fi/avoindata/lisenssi">lisenssi</a>',
+	maxZoom: 17,
+	layers: 'opendata:tampere_okartta_gk24',
+	version: '1.3.0'
+    });
+    baseLayersOfMaps[i].push(treGuideLayer);
+    var treBaseLayer = L.tileLayer.wms('http://opendata.navici.com/tampere/ows?service=wms', {
+	attribution: 'Sisältää Tampereen kaupungin <a href="http://palvelut2.tampere.fi/tietovaranto/tietovaranto.php?id=137&alasivu=1&vapaasana=">"Tampereen kantakartta ilman kiinteistörajoja"</a>-aineistoa, <a href="http://www.tampere.fi/avoindata/lisenssi">lisenssi</a>',
+	maxZoom: 17,
+	layers: 'opendata:tampere_kkartta_pohja_gk24',
+	version: '1.3.0'
+    });
+    baseLayersOfMaps[i].push(treBaseLayer);
+}
+
+var baseMaps = {
+    "Perinteinen, OpenStreetMap": baseLayersOfMaps[1][0],
+    "Opaskartta, Tampere": baseLayersOfMaps[1][5],
+    "Kantakartta, Tampere": baseLayersOfMaps[1][6],
+    "Ilmakuva, MML / kartat.kapsi.fi": baseLayersOfMaps[1][4],
+    "Katu, MapQuest": baseLayersOfMaps[1][2],
+    "Pyöräily, Thunderforest": baseLayersOfMaps[1][1],
+    "Mustavalko, Stamen": baseLayersOfMaps[1][3]
+}
 
 $(document).ready(function(){
 
@@ -71,26 +113,50 @@ Meluselvitys, Tampereen kaupunki, <a href="http://www.tampere.fi/avoindata/lisen
     });
     */
     var mapDay2012 = L.map('div1', {
-	layers: [mmlTaustaLayer1, noiseLayerDay2012],
+	layers: [baseLayersOfMaps[0][0], noiseLayerDay2012],
 	attributionControl: false,
         zoomControl: false
     }).setView([INITIAL_LAT, INITIAL_LON], 12);
     var mapNight2012 = L.map('div2', {
-	layers: [mmlTaustaLayer2, noiseLayerNight2012],
+	layers: [baseLayersOfMaps[1][0], noiseLayerNight2012],
         attributionControl: false,
         zoomControl: false
     }).setView([INITIAL_LAT, INITIAL_LON], 12);
     var mapDay2030 = L.map('div3', {
-	layers: [mmlTaustaLayer3, noiseLayerDay2030],
+	layers: [baseLayersOfMaps[2][0], noiseLayerDay2030],
         attributionControl: false,
         zoomControl: false
     }).setView([INITIAL_LAT, INITIAL_LON], 12);
     var mapNight2030 = L.map('div4', {
-	layers: [mmlTaustaLayer4, noiseLayerNight2030],
+	layers: [baseLayersOfMaps[3][0], noiseLayerNight2030],
         attributionControl: true,
         zoomControl: false
     }).setView([INITIAL_LAT, INITIAL_LON], 12);
 
+    function baseMapChage(e) {
+	for (var i = 0; i < baseLayersOfMaps[1].length; i++) {
+	    if (baseLayersOfMaps[1][i] == e.layer) {
+		mapDay2012.removeLayer(baseLayersOfMaps[0][currentBaseLayerIndex]);
+		mapDay2030.removeLayer(baseLayersOfMaps[2][currentBaseLayerIndex]);
+		mapNight2030.removeLayer(baseLayersOfMaps[3][currentBaseLayerIndex]);
+		mapDay2012.addLayer(baseLayersOfMaps[0][i]);
+		mapDay2030.addLayer(baseLayersOfMaps[2][i]);
+		mapNight2030.addLayer(baseLayersOfMaps[3][i]);
+		currentBaseLayerIndex = i;
+		break;
+	    }
+	}
+	noiseLayerDay2012.bringToFront();
+	noiseLayerNight2012.bringToFront();
+	noiseLayerDay2030.bringToFront();
+	noiseLayerNight2030.bringToFront();
+    };
+
+    mapNight2012.on('baselayerchange', baseMapChage);
+
+    L.control.layers(baseMaps).addTo(mapNight2012);
+    noiseLayerNight2012.bringToFront();
+    
     mapNight2012.addControl(L.control.zoom( { position: 'topright' } ));
     
     L.Control.setOpacityControlMap(mapNight2012);
